a6bf93bc3b2b0c3b6dc8037287fa81647fd407bd
harshswami385 <harshswami138@kgpian.iitkgp.ac.in>
2026-04-17 02:04:36 +0530

    Normalize Firebase private key newlines from env

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,9 @@ async function bootstrap() {
       admin.initializeApp({
         credential: admin.credential.cert({
           projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
-          privateKey: configService.get<string>('FIREBASE_PRIVATE_KEY'),
+          privateKey: configService
+            .get<string>('FIREBASE_PRIVATE_KEY')
+            ?.replace(/\\n/g, '\n'),
           clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
         }),
       });
